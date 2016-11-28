0e0e041d8882ccb7c651ea56095ed34abd315ab3
MarkIsNotTaken <mdemesillo@gmail.com>
2016-11-28 08:45:38 -1000

    loop_city.js

---
+++ loop_city.js
@@ -0,0 +1,12 @@
+var count;
+
+for (count = 0; count < 101; count++){
+  console.log(count);
+}
+
+var animalCount;
+
+for (animalCount = 0; animalCount < 6; animalCount++){
+  console.log("There are " + animalCount + " animals in Noahs Arc");
+}
+
