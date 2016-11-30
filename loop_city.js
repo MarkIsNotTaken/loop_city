6b1680b2a57fddc077cf23b472470fa673f6b5ce
MarkIsNotTaken <mdemesillo@gmail.com>
2016-11-30 08:55:17 -1000

    loop_city.js

--- loop_city.js
+++ loop_city.js
@@ -16,6 +16,21 @@ for (var animalia = 0; animalia < 5; animalia++){
   console.log(partyAnimal[animalia]);
 }
 
+var weaponChest = ["Mace", "Sorcerer's Stone", "Excalibur"]
 
+var hero = {
+  wizard : "Merlin",
+  weapon : weaponChest[1],
+  power : 30
+}
+
+function kingdom(arty){
+  if(hero.wizard == "Merlin" && hero.weapon == "Sorcerer's Stone"){
+    console.log(hero.wizard + " shall slay the dragon with the " + hero.weapon)
+    console.log(hero.wizard + " is ready for war with the " + hero.weapon)
+	}
+}
+
+kingdom();
 
 
